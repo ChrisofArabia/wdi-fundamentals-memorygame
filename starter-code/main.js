d0028d9932e3532db53a8e1625feb34df97f28d7
chrisofarabia <chrisofarabia@yahoo.co.uk>
2016-10-24 20:33:53 +0100

    Added card match logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,13 @@
 // console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 
+if (cardOne === cardThree ) {
+  window.alert( 'You found a match!' );
+}
+else {
+  window.alert( 'Sorry, try again.' );
+}
