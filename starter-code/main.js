3c309cfec13619df1f775e17973d20f425f44a9d
chrisofarabia <chrisofarabia@yahoo.co.uk>
2016-10-28 18:30:08 +0100

    Cards created using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,25 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
+/*
 if (cardOne === cardThree ) {
   window.alert( 'You found a match!' );
 }
 else {
   window.alert( 'Sorry, try again.' );
 }
+*/
+
+
+
+var createCards = function() {
+  var gameBoard = document.getElementById( 'game-board' );
+
+  for( i = 0; i < 4; i++) {
+    newDiv = document.createElement('div');
+    newDiv.className = 'card';
+    gameBoard.appendChild(newDiv);
+  }
+}
+
+createCards();
